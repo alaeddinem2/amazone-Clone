8466b4cdc360d67b0d211466a40b519875d67f73
alaeddine <telliala@gmail.com>
2023-12-13 12:25:48 +0100

    remove from cart

--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import OrderList,checkout,add_to_cart
+from .views import OrderList,checkout,add_to_cart,remove_from_cart
 
 app_name = 'order'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('',OrderList.as_view()),
     path('checkout/',checkout),
     path('add-to-cart',add_to_cart,name="add-to-cart"),
+    path('<int:id>/remove-from-cart',remove_from_cart),
 ]
